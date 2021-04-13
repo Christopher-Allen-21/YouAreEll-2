1edd870ca4a476a9f65ec1b331ccbd0b0d82ef79
Chris Allen <callen7908@yahoo.com>
2021-04-13 19:34:00 -0400

    No more squiggles

--- Client/src/main/java/controllers/ServerController.java
+++ Client/src/main/java/controllers/ServerController.java
@@ -1,32 +1,40 @@
 package controllers;
 
-import spiffyUrlManipulator;
+//import spiffyUrlManipulator;
 
-public class ServerController() {
+import models.Id;
+
+public class ServerController {
     private String rootURL = "http://zipcode.rocks:8085";
 
-    private ServerController svr = new ServerController();
+    private static ServerController svr = new ServerController();
 
     private ServerController() {}
+    //private constructor which makes this a singleton as no one can make an instance anywhere but within this class
+    //common design pattern to make the controllers singletons where there's only one handling stuff; also common to have try catch on private constructor as we don't want the program to run if it doesn't get instantiated
+    //everything that is static belongs in memory from the beginning;
 
-    public static shared() {
+    public static ServerController shared() {
         return svr;
     }
 
-    public JsonString idGet() {
+    public String idGet() {
         // url -> /ids/
         // send the server a get with url
         // return json from server
+        return null;
     }
-    public JsonString idPost(Id) {
+    public String idPost(Id id) {
         // url -> /ids/
         // create json from Id
         // request
         // reply
         // return json
+        return null;
     }
-    public JsonString idPut(Id) {
+    public String idPut(Id id) {
         // url -> /ids/
+        return null;
     }
 
 
--- Client/src/main/java/controllers/TransactionController.java
+++ Client/src/main/java/controllers/TransactionController.java
@@ -1,6 +1,7 @@
 package controllers;
 
 import models.Id;
+import org.apache.http.impl.client.CloseableHttpClient;
 
 import java.util.List;
 
@@ -8,15 +9,20 @@ public class TransactionController {
     private String rootURL = "http://zipcode.rocks:8085";
     private MessageController msgCtrl;
     private IdController idCtrl;
+    private CloseableHttpClient transactionClient;
 
     public TransactionController(MessageController m, IdController j) {}
 
     public List<Id> getIds() {
-
+        return null;
     }
     public String postId(String idtoRegister, String githubName) {
         Id tid = new Id(idtoRegister, githubName);
         tid = idCtrl.postId(tid);
         return ("Id registered.");
+    }
+
+    public String makecall(String s, String get, String s1) {
+        return null;
     }
 }
